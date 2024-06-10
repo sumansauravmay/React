fcf3488700466f41e99cd46c30461d2bb325434a
anandgithub011 <anandany830@gmail.com>
2024-06-10 21:46:18 +0530

    updatecounter by Aanand

--- updatecounter/src/App.jsx
+++ updatecounter/src/App.jsx
@@ -4,24 +4,26 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleClick=()=>{
-    setCount(count-1);
-    setCount(count-1);
-  }
+  const handleClick = () => {
+    setCount(count - 1);
+    setCount(count - 1);
+  };
 
-  const handleIncClick=(val)=>{
-    setCount((prev)=>prev+val);
-    setCount((prev)=>prev+val);
-    setCount((prev)=>prev+val);
-    setCount((prev)=>prev+val);
-    setCount((prev)=>prev+val);
-  }
+  const handleIncClick = (val) => {
+    setCount((prev) => prev + val);
+    setCount((prev) => prev + val);
+    setCount((prev) => prev + val);
+  };
 
   return (
     <>
       <h1>Count: {count}</h1>
-      <button disabled={count===0} onClick={handleClick}>Dec</button>
-      <button disabled={count>=50} onClick={()=>handleIncClick(1)}>Inc</button>
+      <button disabled={count === 0} onClick={handleClick}>
+        Dec
+      </button>
+      <button disabled={count >= 50} onClick={() => handleIncClick(1)}>
+        Inc
+      </button>
     </>
   );
 }
