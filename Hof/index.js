9f67732b2a61a3dc742f6ee30cc1df05e82e56f0
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-16 00:18:37 +0530

    HOF

--- Hof/index.js
+++ Hof/index.js
@@ -27,8 +27,9 @@
 
 // let str2 = [1, 2, 5, 6, 3, 5];
 // let newArr = [];
-// str2.forEach((el) => {
-//   newArr.push(el*2)
+// str2.forEach((el, i) => {
+//   newArr.push(el*2);
+//   console.log(i)
 // //   console.log(newArr);
 // });
 
@@ -36,12 +37,6 @@
 
 // tc: O(n)
 // sc: O(n)
-
-
-
-
-
-
 
 // filter: return a new Array which verrify;
 
@@ -52,19 +47,105 @@
 //     return el[0]=="d" && el[el.length-1]=="h"
 // })
 // console.log(w);
-
-
+// tc: O(n)
+// sc: O(1)
 
 // reduce: give a single value or summarise the value
-let arr=[2,3,7,8,3,6];
+// let arr=[2,3,7,8,3,6];
 
 // let x=arr.reduce((initial, element)=>{
 //     return initial+element+" ";
 // },[]);
 // console.log(x);
 
+// let x=arr.reduce((initial, element, index)=>{
+//     return initial+element;
+// },0);
+// console.log(x);
 
-let x=arr.reduce((initial, element)=>{
-    return initial+element;
-},0);
-console.log(x);
+// tc: O(n)
+// sc: O(1)
+
+// #Chaining
+
+// 1.  Find the sum which are odd.
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// let x = arr
+//   .filter((el) => {
+//     return el % 2 === 1;
+//   })
+//   .reduce((init, e) => {
+//     return init + e;
+//   }, 0);
+// console.log(x);
+
+// tc: O(n^2)
+// sc: O(1)
+
+// 2. print the sum of length, whose length is odd.
+
+let arr = ["John", "Victor", "Wilson", "Ram", "Saurav", "Suman"];
+//first method:
+// let x = arr
+//   .filter((el) => {
+//     return el.length % 2 === 1;
+//   })
+//   .map((el) => {
+//     return el.length;
+//   })
+//   .reduce((initial, el) => {
+//     return initial + el;
+//   }, 0);
+// console.log(x);
+// tc: O(n^3)
+// sc: O(1)
+
+//second method:
+// let x = arr
+//   .filter((el) => {
+//     return el.length % 2 === 1;
+//   })
+//   .reduce((initial, el) => {
+//     return initial + el.length;
+//   }, 0);
+// console.log(x);
+
+// tc: O(n^2)
+// sc: O(1)
+
+// 3. print the full name who is from India
+
+let users = [
+  { firstName: "Suman", lastName: "Saurav", place: "India" },
+  { firstName: "Ram", lastName: "Kumar", place: "India" },
+  { firstName: "Wilson", lastName: "Victor", place: "USA" },
+  { firstName: "John", lastName: "Michael", place: "Australia" },
+];
+//first method
+//  users
+//   .filter((el) => {
+//     return el.place === "India";
+//   })
+//   .map((el) => {
+//     return el.firstName + " " + el.lastName;
+//   })
+//   .forEach((el)=>{
+//     console.log(el)
+//   })
+// tc: O(n^3)
+// sc: O(1)
+
+//second method
+
+users
+  .filter((el) => {
+    return el.place === "India";
+  })
+  .forEach((el) => {
+    console.log(el.firstName + " " + el.lastName);
+  });
+
+// tc: O(n^2)
+// sc: O(1)
+
