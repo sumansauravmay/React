3a4381617963a4825fb758d64f36608865683831
sumansauravmay <sumansauravmay12345@gmail.com>
2024-09-10 19:50:53 +0530

    react install manually

---
+++ react_install_manually/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }],
+      },
+    ],
+  },
+};
+
+
