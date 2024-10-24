0edc311599bcede956149c07f65a5f1997cb71c4
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-24 17:21:24 +0530

    context provider

--- Context_API/todo/src/App.js
+++ Context_API/todo/src/App.js
@@ -2,14 +2,13 @@
 import './App.css';
 import Todo from './todo/Todo';
 import TodoItem from './todo/TodoItem';
-import TodoProvider from './todo/TodoProvider';
 
 function App() {
   return (
-    <TodoProvider>
+    <>
      <Todo/>
      <TodoItem/>
-    </TodoProvider>
+    </>
   );
 }
 
